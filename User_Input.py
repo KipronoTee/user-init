97f0de3b32b717a75918668a1a0ba7845be4dbf3
KipronoTee <45683678+KipronoTee@users.noreply.github.com>
2024-03-01 16:46:52 +0300

    Update User_Input.py

---
+++ User_Input.py
@@ -0,0 +1,15 @@
+# ask the user their name
+name = input('what is your name? ')
+
+# say hello to useer
+print ('hello,' + name)
+
+# ask the user what their age is
+age = input('what is your age? ')
+
+# ask the use their location
+location = input('what is your location? ')
+
+
+# sing user name, age and location
+print("hello", name, "you_are", age, "and_you_live in", location)
